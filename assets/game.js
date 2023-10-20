208f3efd29a9667509300d15a8ee61d7272a084b
Chiara Manni <chiaramiren@gmail.com>
2023-10-20 18:45:41 +0200

    viernes tarde

--- assets/game.js
+++ assets/game.js
@@ -8,9 +8,30 @@ const Game = {
   background: undefined,
   player: undefined,
 
+  keys: { JUMP: 'ArrowUp', LEFT: "ArrowLeft", RIGHT: "ArrowRight" },
+
   init() {
     this.start();
   },
+
+  setEventListeners() {
+    "keydown", key => {
+      switch (key.code) {
+        case this.keys.JUMP:
+          this.player.JUMP
+          break;
+        case this.keys.LEFT:
+          this.player.LEFT
+          break;
+        case this.keys.RIGHT:
+          this.player.RIGHT
+          break
+      }
+    }
+
+
+  }
+
 
   start() {
     this.createElements();
